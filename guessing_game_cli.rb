cf42f3cc4840f6bcf3273bf84cb99e97435e1b51
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-11 15:15:31 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,10 +1,8 @@
 def run_guessing_game
   correct_number = rand(6) + 1
-  get_user_input = gets.chomp
-  prompt_user = puts 'Guess a number between 1 and 6'
   
-  prompt_user
-  get_user_input
+  puts 'Guess a number between 1 and 6'
+  get_user_input = gets.chomp
   
   if get_user_input != "#{correct_number}"
     puts "Sorry! The computer guessed #{correct_number}."
